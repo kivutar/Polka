495c0a87f7b706e363f021282402e7a631c50bad
Jean-André Santoni <jean.andre.santoni@gmail.com>
2010-10-17 16:16:28 +0200

    Initial commit

---
+++ polka.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+use Dancer;
+use File::Find;
+use Text::Markdown 'markdown';
+
+my %cache;
+my $dirtree = dirtree( 'data' );
+sub slurp { local $/; open(my $fh, '<:utf8', shift); <$fh> }
+sub cache { my $f = shift; $cache{$f} ||= slurp $f }
+
+sub dirtree {
+    my $dir = shift;
+    my %tree;
+
+    opendir(DIR, $dir);
+    my @files = grep {!/^\.\.?$/} readdir(DIR);
+    closedir(DIR);
+
+    $tree{"$dir/$_"} = -d "$dir/$_" ? dirtree("$dir/$_") : '' for @files;
+
+    \%tree;
+}
+
+sub menu {
+    my ($tree, $path, $unfolded) = @_;
+    my $menu = $unfolded ? "<ul class=\"active\">\n" : "<ul>\n";
+    while ( my ($link, $child) = each %$tree ) {
+        $link =~ s/^data\///;
+        my $label = pop @{[ split '/', $link ]};
+        if ( $child ) {
+            $menu .= "<li class=\"dir\">$label\n".menu($child, $path, $path =~ /$link/)."</li>\n";
+        } else {
+            my $class = $link eq $path ? ' class="active"' : '';
+            $menu .= "<li$class><a href=\"/$link\">$label</a></li>\n";
+        }
+    }
+    $menu .= "</ul>\n";
+}
+
+get qr{/(?<path>.*)} => sub {
+    return cache captures->{path} if captures->{path} =~ /static/;
+    captures->{path} ||= 'Home';
+    "<html>
+        <head>
+            <link href=\"/static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
+        </head>
+        <body>
+            <div>
+                <h1><a href=\"/\"><span id=\"main-title\">Polka~</a></h1>
+                <nav>" . menu( $dirtree, captures->{path} ) . "</nav>
+                <div id=\"page\">" . markdown( cache 'data/'.captures->{path} ) . "</div>
+            </div>
+            <footer>Powered by <a href=\"#\">Polka</a>.</footer>
+        </body>
+    </html>";
+};
+
+dance;
